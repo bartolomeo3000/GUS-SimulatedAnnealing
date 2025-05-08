cf3bfbb70d1fed9dda69a66f59856443caaea63f
FSalinski <franek.salinski@gmail.com>
2025-05-08 16:14:55 +0200

    Implement simulated annealing

---
+++ simulated_annealing.R
@@ -0,0 +1,128 @@
+library(stratallo)
+
+generate_test_data <- function(H = 5, pop = 500, seed = 42) {
+  set.seed(seed)
+  
+  # Losowanie proporcji dla każdej warstwy (żeby ich suma = 1)
+  proportions <- runif(H)
+  proportions <- proportions / sum(proportions)
+  
+  # Liczności populacji w warstwach (zaokrąglone, suma = N)
+  N <- round(proportions * pop)
+  
+  # Korekta sumy (jeśli zaokrąglenie ją zmieni)
+  diff <- pop - sum(N)
+  if (diff != 0) {
+    N[which.max(N)] <- N[which.max(N)] + diff
+  }
+  
+  # Współczynniki wariancji A (np. w zakresie 1–10)
+  A <- runif(H, min = 1, max = 10)
+  
+  # Ograniczenia na liczność próby z każdej warstwy
+  # min. 5, max. 40% liczności warstwy
+  m <- pmin(20, N)
+  M <- ceiling(0.1 * N)
+  
+  data <- data.frame(
+    h = 1:H,
+    N = N,
+    A = A,
+    m = m,
+    M = M
+  )
+  
+  return(data)
+}
+
+
+df <- generate_test_data(H = 16, pop = 38000000)
+head(df)
+
+f <- function(A, x){
+  sum(A ** 2 / x)
+}
+
+generate_state_0 <- function(m, M, n, H){
+  x <- m
+  
+  k <- n - sum(m)
+  while (k > 0){
+    i <- sample(1:H, 1)
+    if (x[i] + 1 < M[i]){
+      x[i] <- x[i] + 1
+      k <- k - 1
+    }
+  }
+  x
+}
+
+SA <- function(A, m, M, n, beta = 0.95){
+  H <- length(A)
+  t <- 1
+  
+  if (n < sum(m)){
+    return(FALSE)  
+  }
+  if (n > sum(M)){
+    return (FALSE)
+  }
+  
+  x <- generate_state_0(m, M, n, H)
+  x
+  f_prev <- f(A, x)
+  
+  k <- 0 # Licznik pozostawania w tym samym stanie
+  K <- 100
+  
+  while (k < K){
+    while (TRUE){
+      w <- sample(1:H, 2)
+      if (m[w[1]] > x[w[1]] - 1){
+        next
+      }
+      if (x[w[2]] + 1 > M[w[2]]){
+        next
+      }
+      x[w[1]] <- x[w[1]] - 1
+      x[w[2]] <- x[w[2]] + 1
+      
+      f_next <- f(A, x)
+      
+      delta <- f_next - f_prev
+      if (delta < 0){
+        f_prev <- f_next
+        t <- t * beta
+        k <- 0
+        break
+      }
+      
+      prob <- exp(-delta / t)
+      u <- runif(1)
+      
+      if (u < prob){
+        f_prev <- f_next
+        t <- t * beta
+        k <- 0
+        break
+      }
+      # Zostajemy w tym samym stanie, wiec odwracamy przejście
+      x[w[1]] <- x[w[1]] + 1
+      x[w[2]] <- x[w[2]] - 1
+      k <- k + 1
+      
+      t <- t * beta
+      break
+    }
+  }
+  x
+}
+
+x0 <- SA(df$A, df$m, df$M, 1000)
+f(df$A, x0)
+
+x1 <- generate_state_0(df$m, df$M, 1000, 16)
+f(df$A, x1)
+
+xrna <- rnabox(1000, df$A, df$M, df$m)
+f(df$A, xrna)
