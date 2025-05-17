9a40441860daf3930b681b17b57f7a976a9f2955
Bruno Podbielski <brunopodbielski.01@gmail.com>
2025-05-17 12:37:54 +0200

    Added gen_state_0 for 2 layer case

--- simulated_annealing_2_layers.R
+++ simulated_annealing_2_layers.R
@@ -1,36 +1,38 @@
-f <- function(M, N, D, S, l){
+f <- function(M, N, D, S, l, m, n){
   b <- sum((1/m - 1/M) * M**2 * D**2)
-  
   for (h in 1:length(M)){
     alpha <- M[h] / m[h]
-    for (j in 1:lenght(M[h])){
-      b <- b + alpha * (1/n[h][j] - 1/N[h][j]) * N[h][j]**2 * S[h][j]**2
+    for (j in 1:length(M[h])){
+      b <- b + alpha * (1/n[[h]][j] - 1/N[[h]][j]) * N[[h]][j]**2 * S[[h]][j]**2
     }
   }
   b
 }
 
-generate_data <- function(H = 5, sumM = 50, pop = 10000, minN = 10){
+check <- function(totm, totn, m, n, M){
+  sumn <- 0
+  for(h in 1:length(m)){
+    sumn <- sumn+sum(n[[h]])*m[h]/M[h]
+  }
+  if(sum(m)==totm & sumn<totn+1 & sumn>totn-1){return(TRUE)}
+  return(FALSE)
+}
+
+generate_test_data <- function(H = 5, sumM = 50, pop = 10000, minN = 10){
   M <- rep(1, H)
   sumM <- sumM - H
-  
   for (i in 1:sumM){
     s <- sample(1:5, 1)
     M[s] <- M[s] + 1
   }
-  
   N <- list()
-  
   for (i in 1:H){
     N[[i]] <- rep(minN - 1, M[i])
   }
-  
   pop <- pop - (sumM + H) * (minN - 1)
-  
   p <- sample(1:(pop - 1), sumM + H - 1)
   p <- sort(p)
   p <- c(p[1], diff(p), pop - p[length(p)])
-  
   l <- 1
   for (i in 1:H){
     for (j in 1:M[i]){
@@ -38,26 +40,66 @@ generate_data <- function(H = 5, sumM = 50, pop = 10000, minN = 10){
       l <- l + 1
     }
   }
-  
   D <- runif(H, 1, 10)
-  
   S <- list()
   for (i in 1:H){
     S[[i]] <- runif(M[i], 1, 10)
   }
-  
   return(list(M, N, D, S))
 }
-
-a <- generate_data()
+a <- generate_test_data()
 M <- a[[1]]
 N <- a[[2]]
 D <- a[[3]]
 S <- a[[4]]
 
-generate_state <- 
+generate_state_0 <- function(m_ch, M, n_ch, N, H){
+  while(TRUE){
+    m_tmp <- m_ch
+    m <- rep(0,H)
+    while(m_tmp>0){
+      s <- sample(1:H, 1, prob=M) # wagi jak proporcje w M
+      if(m[s]<M[s]){m[s] <- m[s]+1; m_tmp <- m_tmp-1}
+    }
+    dol <- rep(0, H)
+    gora <- rep(0, H)
+    for(i in 1:H){
+      dol[i] <- sum(sort(N[[i]])[1:m[i]])
+      gora[i] <- sum(sort(N[[i]], decreasing=T)[1:m[i]])
+    }
+    if(floor(sum((m/M)*dol)) < n_ch & floor(sum((m/M)*gora)) > n_ch){break}
+  }
+  while(sum((m/M)*gora)-n_ch>0){
+    s <- sample(1:H, 1)
+    if(gora[s]-1>=dol[s]){gora[s] <- gora[s]-1}
+  }
+  n <- list()
+  for (i in 1:H){
+    n[[i]] <- rep(0, M[i])
+  }
+  for(i in 1:H){
+    while(TRUE){
+      s <- sample(1:length(N[[i]]), m[i])
+      if(sum(N[[i]][s])>=gora[i]){
+        g <- gora[i]
+        while(g>0){
+          c <- sample(s, 1) # możemy wylosować szkołę z której nie weźmiemy uczniów, ale zakładam że o to chodzi w zadaniu
+          if(n[[i]][c]+1<=N[[i]][c]){
+            n[[i]][c] <- n[[i]][c]+1
+            g <- g-1
+          }
+        }
+        break}
+    }
+  }
+  return(list(m,n))
+}
+b <- generate_state_0(25,M,5000,N,5)
+m <- b[[1]]
+n <- b[[2]]
 
-f(M, N, D, S, 500)
 
 
-
+f(M, N, D, S, 500, m, n)
+check(25,5000,m,n,M)
+sum(unlist(n))
